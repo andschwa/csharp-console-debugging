e8fa0d9d86dbd5d85cabe99147ac4a6eb6213c2c
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-05-16 15:15:17 -0700

    Encodings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace ConsoleApplication
 {
@@ -6,22 +9,41 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            String[] lines = {
-                $"BufferHeight: {Console.BufferHeight}",
-                $"BufferWidth: {Console.BufferWidth}",
-                $"WindowHeight: {Console.WindowHeight}",
-                $"WindowWidth: {Console.WindowWidth}",
-                $"LargestWindowHeight: {Console.LargestWindowHeight}",
-                $"LargestWindowWidth: {Console.LargestWindowWidth}",
-                $"IsErrorRedirected: {Console.IsErrorRedirected}",
-                $"IsOutputRedirected: {Console.IsOutputRedirected}",
-                $"IsInputRedirected: {Console.IsInputRedirected}",
-                ""
-            };
-            foreach (var line in lines) {
-                Console.Out.WriteLine(line);
+            String[] lines =
+                {
+                    $"BufferHeight: {Console.BufferHeight}",
+                    $"BufferWidth: {Console.BufferWidth}",
+                    $"WindowHeight: {Console.WindowHeight}",
+                    $"WindowWidth: {Console.WindowWidth}",
+                    $"LargestWindowHeight: {Console.LargestWindowHeight}",
+                    $"LargestWindowWidth: {Console.LargestWindowWidth}",
+                    $"IsErrorRedirected: {Console.IsErrorRedirected}",
+                    $"IsOutputRedirected: {Console.IsOutputRedirected}",
+                    $"IsInputRedirected: {Console.IsInputRedirected}",
+                    ""
+                };
+
+            using (var stream = File.CreateText("Default.log"))
+            {
+                foreach (var line in lines) { stream.WriteLine(line); }
             }
-            Console.Error.WriteLine("This is the error stream.");
+
+            var encodings = new Dictionary<String, Encoding>()
+                {
+                    { "UTF8-Default.log", new UTF8Encoding() },
+                    { "ASCII.log", new ASCIIEncoding() },
+                    { "UTF8-ExplicitBOM.log", new UTF8Encoding(true) },
+                    { "Unicode.log", new UnicodeEncoding() },
+                };
+
+            foreach (var encoding in encodings)
+            {
+                using (var file = new FileStream(encoding.Key, FileMode.Create))
+                using (var stream = new StreamWriter(file, encoding.Value))
+                {
+                    foreach (var line in lines) { stream.WriteLine(line); }
+                }
+            }
         }
     }
 }
