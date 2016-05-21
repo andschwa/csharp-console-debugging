6cde436e41d57a71e23b88b015c6cf6555dfc4fd
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-05-20 18:09:15 -0700

    Add working failing test

--- Program.cs
+++ Program.cs
@@ -1,41 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+using Xunit;
 
-namespace ConsoleApplication
+namespace TestNamespace
 {
-    public class Program
+    public class TestClass
     {
-        public static void Main(string[] args)
+        [Fact]
+        public void ThisWillFail()
         {
-            String[] lines =
-                {
-                    "Normal ASCII text",
-                    "你好"
-                };
-
-            using (var stream = File.CreateText("Default.log"))
-            {
-                foreach (var line in lines) { stream.WriteLine(line); }
-            }
-
-            var encodings = new Dictionary<String, Encoding>()
-                {
-                    { "UTF8-Default.log", new UTF8Encoding() },
-                    { "ASCII.log", new ASCIIEncoding() },
-                    { "UTF8-ExplicitBOM.log", new UTF8Encoding(true) },
-                    { "Unicode.log", new UnicodeEncoding() },
-                };
-
-            foreach (var encoding in encodings)
-            {
-                using (var file = new FileStream(encoding.Key, FileMode.Create))
-                using (var stream = new StreamWriter(file, encoding.Value))
-                {
-                    foreach (var line in lines) { stream.WriteLine(line); }
-                }
-            }
+            Assert.True(false);
         }
     }
 }
